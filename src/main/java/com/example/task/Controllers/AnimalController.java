43358b5cb20379735914a109d40b932b1b5b7e29
Bartłomiej Wieleba <barwieleba@gmail.com>
2020-11-08 17:35:29 +0100

    created working get and post requests

--- src/main/java/com/example/task/Controllers/AnimalController.java
+++ src/main/java/com/example/task/Controllers/AnimalController.java
@@ -35,18 +35,18 @@ public class AnimalController {
     }
 
     @PutMapping("/animals/{id}")
-    AnimalEntity replaceAnimal(@RequestBody AnimalEntity newAnimal, @PathVariable Long id){
+    AnimalEntity replaceAnimal(@RequestBody List<AnimalEntity> list, @PathVariable Long id){
         return repository.findById(id)
                 .map(animalEntity -> {
-                    animalEntity.setName(newAnimal.getName());
-                    animalEntity.setSpecie(newAnimal.getSpecie());
-                    animalEntity.setBreed(newAnimal.getBreed());
-                    animalEntity.setAge(newAnimal.getAge());
+                    animalEntity.setName(list.get(0).getName());
+                    animalEntity.setSpecie(list.get(0).getSpecie());
+                    animalEntity.setBreed(list.get(0).getBreed());
+                    animalEntity.setAge(list.get(0).getAge());
                     return repository.save(animalEntity);
                 })
                 .orElseGet(()->{
-                    newAnimal.setId(id);
-                    return repository.save(newAnimal);
+                    list.get(0).setId(id);
+                    return repository.save(list.get(0));
                 });
     }
 
